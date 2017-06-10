b3e40536627c3c654a8c6467f0f7252801ba9963
lotny <tomasz.trela87@gmail.com>
2017-06-10 21:25:40 +0200

    updating info, some fixes

--- msgs.js
+++ msgs.js
@@ -5,36 +5,38 @@ $("#help_content").html(pickHelp(topic));
 }
 
 function pickHelp(topic){
+	var helpMessage = "";
 	//switch based on context
 	switch(topic){
 	case("languages"):
-	return "<p>The square buttons let you pick languages to be displayed in the table.</p>";
+	helpMessage = "<p>The square buttons let you pick languages to be displayed in the table.</p>";
 	break;
 	case("filter"):
-	return "<p>The filter lets you search for words that are already in the table. To filter words in a particular column, type its name before a colon. E.g., \"noun:fox\".</p>";
+	helpMessage = "<p>The filter lets you search for words that are already in the table. To filter words in a particular column, type its name before a colon. E.g., \"noun:fox\".</p>";
 	break;
 	case("edit mode"):
-	return "<p>Edit mode lets you modify and add data.<br>Saving changes requires a login and a password.</p>";
+	helpMessage = "<p>Edit mode lets you modify and add data.<br>Saving changes requires a login and a password.</p>";
 	break;
 	case("sql mode"):
-	return "<p>Search mode will let you find a word in all available languages.<br>This mode has not yet been implemented.</p>";
+	helpMessage = "<p>Search mode will let you find a word in all available languages.<br>This mode has not yet been implemented.</p>";
 	break;
 	case("compare mode"):
-	return "<p>Compare mode displays similar words present in all chosen languages.<br>In case of errors, try to pick fewer languages.</p>";
+	helpMessage = "<p>Compare mode displays similar words present in all chosen languages.<br>In case of errors, try to pick fewer languages.</p>";
 	break;
 	case("table"):
-	return "<p>The table displays the result of the query. In edit mode, you can double-click cells of the table to edit them.</p>";
+	helpMessage = "<p>The table displays the result of the query. In edit mode, you can double-click cells of the table to edit them.</p>";
 	break;
 	case("changed"):
-	return "The selected cell is changed. It will be saved when you press the <b>save changes</b> button. Click on this cell to change its status.";
+	helpMessage = "The selected cell is changed. It will be saved when you press the <b>save changes</b> button. Click on this cell to change its status.";
 	break;
 	case("cancelled"):
-	return "The selected cell is changed but is not active. It will not be saved when you press the <b>save changes</b> button. Click on this cell to change its status.";
+	helpMessage = "The selected cell is changed but is not active. It will not be saved when you press the <b>save changes</b> button. Click on this cell to change its status.";
 	break;
 	default:
-	return "<p>Click on UI elements to find out more about them.<br>Click <b>?</b> to hide this pane.<br>Go <a href='changelog.html' >here</a> to see the list of changes and coming features.</p>";
+	helpMessage = "<p>Click on UI elements to find out more about them.<br>Click <b>?</b> to hide this pane.<br>Go <a href='changelog.html' >here</a> to see the list of changes and coming features.</p>";
 	break;
-	}
+}
+return helpMessage;
 }
 
 
