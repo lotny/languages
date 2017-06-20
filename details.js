2f8c468f217061ab804a4fa20c0549468e310355
lotny <tomasz.trela87@gmail.com>
2017-06-20 22:40:35 +0200

    adding etymology to details window (oxford api)

--- details.js
+++ details.js
@@ -25,6 +25,11 @@ div.id = "detailsAdditional";
 $("#details").append(div);
 
 var div = document.createElement("div");
+div.id = "detailsEtymology";
+$("#details").append(div);
+
+
+var div = document.createElement("div");
 div.id = "detailsClose";
 div.align = "right";
 div.innerHTML = "<button onClick='detailsWindow.close()' class='btn_mode_controls'>close</button>";
@@ -44,7 +49,7 @@ detailsWindow.changeHeader = function(){
 }
 
 
-function getDetails(Id, word, languageId){
+function getDetails(Id, word, columnId){
 	console.log(Id);
 	var db = window.indexedDB.db;
 	
@@ -53,31 +58,56 @@ function getDetails(Id, word, languageId){
 	//alert("name " + event.target.result.Polish);
 	//};
 var detail = "no data found :(";
-var q = context[languageId][0] + ";" + Id;
+var etymology = "";
+var q = context[columnId][0] + ";" + Id;
+var languageId = context[columnId][3];
 console.log(q);
 
 //detailsWindow.clear();
 
 $("#detailsType").html("<span class='detType'>--</span>");
 $("#detailsAdditional").empty();
-$.getJSON('details.php', {'userquery': q}, function(e) { 
-	if (e.result[0] != "ERROR"){
-	detail = e.result[0]['Details'];
-	console.log("result: " + e.result[0]);
-	$("#detailsType").html("<span class='detType'>" + e.result[0]['Type'] + "</span>");
-	if (detail != ""){
-		$("#detailsAdditional").html("<br><br>Detailed information: <br>" + detail);
+$("#detailsEtymology").empty();
+
+var userquery = {
+	wordId: word,
+	languageId: languageId
+}
+console.log(userquery);
+
+$.getJSON('api.php', {'userquery': userquery}, function(e){
+
+if(e.etymology){
+	 	$("#detailsEtymology").html("<br>Etymology: <br>" + e.etymology);
+		//$("#detailsEtymology").html("<audio controls><source src='http://audio.oxforddictionaries.com/en/mp3/house_gb_2.mp3' type='audio/mpeg'></audio>");
+} else {
+	$("#detailsEtymology").empty();
+}
+
+if (e.audio){
+//$("#detailsEtymology").append("<audio controls><source src='" + e.audio+ "' type='audio/mpeg'></audio>");
+
+}
+ }
+);
+
+$.getJSON('details.php', { 'userquery': q }, function (e) {
+	if (e.result[0] != "ERROR") {
+		detail = e.result[0]['Details'];
+		console.log("result: " + e.result[0]);
+		$("#detailsType").html("<span class='detType'>" + e.result[0]['Type'] + "</span>");
+		if (detail != "") {
+			$("#detailsAdditional").html("<br><br>Detailed information: <br>" + detail);
 		} else {
-		$("#detailsAdditional").empty();
+			$("#detailsAdditional").empty();
 		}
 	}
-	else
-	{
-	messageCreate(e.result[1]);
+	else {
+		messageCreate(e.result[1]);
 	}
-clicked = false;
+	clicked = false;
 })
 
 
-$("#detailsHeader").html(word+ " (" + context[languageId][2] + ")" + " [" + Id + "]");
+$("#detailsHeader").html(word+ " (" + context[columnId][2] + ")" + " [" + Id + "]");
 }
